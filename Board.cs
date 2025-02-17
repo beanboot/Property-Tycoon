22a2d328804d64834d4667aba744a7eee979fcd6
beanboot <beepnoop02@gmail.com>
2025-02-17 15:22:08 +0000

    First commit to new branch
    many bugs - DOESNT WORK YET!

--- Board.cs
+++ Board.cs
@@ -6,48 +6,68 @@ public partial class Board : Node2D
 {
 	private uint[] diceRoll;
 	private Sprite2D[] boardSpaces;
+	private Player[] players;
 	private bool canPressButton = true;
+	private int numOfPlayers = 2;
+	private Player currentPlayer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		boardSpaces = new Sprite2D[]
-		{
-			GetNode<Sprite2D>("BoardSpace1"),
-			GetNode<Sprite2D>("BoardSpace2"),
-			GetNode<Sprite2D>("BoardSpace3"),
-			GetNode<Sprite2D>("BoardSpace4"),
-			GetNode<Sprite2D>("BoardSpace5"),
-			GetNode<Sprite2D>("BoardSpace6"),
-			GetNode<Sprite2D>("BoardSpace7"),
-			GetNode<Sprite2D>("BoardSpace8")
+		// Fills boardSpaces array with every space on the board
+		boardSpaces = new Sprite2D[40];
+		int i;
+		for (i = 0; i < 40; i++) {
+			boardSpaces[i] = GetNode<Sprite2D>("BoardSpaces/BoardSpace" + (i+1));
 		};
-		var player = GetNode<Player>("Player");
-		player.player_movement(boardSpaces[0].Position);
+
+		var playerScene = GD.Load<PackedScene>("res://player.tscn");
+		var playerInstance = playerScene.Instantiate();
+		players = new Player[numOfPlayers];
+		for (i = 0; i < numOfPlayers; i++) {
+			playerInstance.Name = ("Player" + (i+1));
+			AddChild(playerInstance);
+			players[i] = GetNode<Player>("Player" + (i+1));
+		}
+		
+		currentPlayer = players[0];
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var playerTextBox = GetNode<RichTextLabel>("CurrentPlayerText");
+		playerTextBox.Text = ("It is currently: ") + currentPlayer.Name + ("'s turn"); 
 	}
 
+	// Called when the Roll Dice button is pressed
 	private async void _on_button_pressed()
 	{
+		// If statement will only run if the player is not currently moving
 		if (canPressButton) {
 			canPressButton = false;
 			var textBox = GetNode<RichTextLabel>("Button/DiceOutput");
 			var player = GetNode<Player>("Player");
 
+			// Rolls two d6s and stores each roll in the diceRoll array, prints result to textbox
 			diceRoll = new uint[] {GD.Randi() % 6 + 1, GD.Randi() % 6 + 1};
 			textBox.Text = Convert.ToString(diceRoll[0]) + ", " + Convert.ToString(diceRoll[1]);
 
+			// targetMoveValue combines each dice roll into one integer
 			int targetMoveValue = Convert.ToInt16(diceRoll[0] + diceRoll[1]);
 		
+			// Iterates the player through the boardSpaces array targetMoveValue times (with a delay)
 			for (int i = 0; i < targetMoveValue; i++) {
-				player.player_movement(boardSpaces[(player.get_pos() + 1) % 8].Position);
-				player.iterate_pos();
-				await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
-			}
+				currentPlayer.player_movement(boardSpaces[(currentPlayer.get_pos() + 1) % 40].Position);
+				currentPlayer.iterate_pos();
+				await ToSignal(GetTree().CreateTimer(0.2f), "timeout");
+
+				/* Potential dynamic delay code:
+				double delay = (0.6 / (targetMoveValue - i)) + 0.1;
+				await ToSignal(GetTree().CreateTimer(delay), "timeout"); */ 
+				
+			};
+
 			canPressButton = true;
 		}
 
--- Player.cs
+++ Player.cs
@@ -4,32 +4,32 @@ using System.Numerics;
 
 public partial class Player : Node2D
 {
+	// CurrentPos is unique to each player and holds the current position of the player
 	private int currentPos;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		currentPos = 0;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
+	// Takes a vector2 as a parameter (p1) and sets the player's spacial position to that of p1
 	public void player_movement(Godot.Vector2 p1)
 	{
 		this.Position = p1;
 		
 	}
 
+	// Iterates the currentPos variable by 1, if it surpasses 40 it goes back to 0
 	public void iterate_pos()
 	{
 		currentPos += 1;
-		if (currentPos > 7) {
+		if (currentPos > 39) {
 			currentPos = 0;
 		};
 	}
 
+	// Returns the player's current position
 	public int get_pos()
 	{
 		return currentPos;
